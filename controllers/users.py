192ce701e85b25f4f71ee97547c28db1fe91e280
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2022-03-31 21:57:29 -0400

    Refactor user controller to be implemented as class

--- controllers/users.py
+++ controllers/users.py
@@ -1,10 +1,23 @@
 from models.user_model import UserModel
 
 
-def get_users():
-    user_model = UserModel()
-    return user_model.get_all_users()
+class UserController:
+    def __init__(self) -> None:
+        self.user_model = UserModel()
 
+    def create_user(self, user_data: dict):
+        return self.user_model.create_user(user_data)
+
+    def get_users(self):
+        # user_model = UserModel()
+        return self.user_model.get_all_users()
+
+    def get_user_info(self, id: int):
+        # user_model = UserModel()
+        return self.user_model.get_user_by_id(id)
+
+    def filter_user_by_first_name(self, first_name: str):
+        return self.user_model.filter_by_keyword({"first_name": first_name})
 
 def get_user_info(id: int):
     user_model = UserModel()
--- routes/api/users.py
+++ routes/api/users.py
@@ -1,18 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from utils.request import ApiResponse, ApiError
-from controllers import users as UserController
+from controllers.users import UserController
 
 bp = Blueprint("users", __name__, url_prefix="/users")
 
 
 @bp.route("/", methods=["GET"])
 def get_users():
-    return ApiResponse(UserController.get_users())
+    user_controller = UserController()
+    user_list = user_controller.get_users()
+    return ApiResponse(user_list)
 
 
 @bp.route("/<int:id>", methods=["GET"])
 def get_user(id: int):
-    user_info = UserController.get_user_info(id)
+    user_controller = UserController()
+
+    user_info = user_controller.get_user_info(id)
 
     if user_info is None:
         return ApiError("User not found", 400)
